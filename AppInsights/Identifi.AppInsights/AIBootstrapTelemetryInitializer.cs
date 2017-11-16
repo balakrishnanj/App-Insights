f4ae5bd5dadec083a6975ba67e8ed3631369d7c0
balakrishnanj <jbalakrishnan@evolenthealth.com>
2017-11-16 13:24:01 -0500

    App-insights-changes

---
+++ AppInsights/Identifi.AppInsights/AIBootstrapTelemetryInitializer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using Identifi.Infrastructure;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using System.Net;
+using System.Web.Configuration;
+
+namespace Identifi.AppInsights
+{
+    public class AIBootstrapTelemetryInitializer: ITelemetryInitializer
+    {
+        private readonly HttpStatusCode[] _detailMessageErrorCodes = { HttpStatusCode.Unauthorized, HttpStatusCode.PreconditionFailed, HttpStatusCode.Forbidden };
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            //Disable Telemetry if the config value is set to true
+            bool disableTelemetry;
+            var configReader = ServiceLocator.Current.GetInstance<IConfigReader>();
+            if (configReader == null)
+            {
+                bool.TryParse(WebConfigurationManager.AppSettings["DisableTelemetry"], out disableTelemetry);
+            }
+            else
+            {
+                disableTelemetry = configReader.Get("DisableTelemetry", false);
+            }
+            if (disableTelemetry)
+                TelemetryConfiguration.Active.DisableTelemetry = true;
+
+            //Treat 403/412/401 as success instead of error
+            var requestTelemetry = telemetry as RequestTelemetry;
+            // Is this a TrackRequest() ?
+            if (requestTelemetry == null) return;
+            int code;
+            bool parsed = Int32.TryParse(requestTelemetry.ResponseCode, out code);
+            if (!parsed) return;
+
+            if (_detailMessageErrorCodes.Contains((HttpStatusCode)code))
+            {
+                // If we set the Success property, the SDK won't change it:
+                requestTelemetry.Success = true;
+                // Allow us to filter these requests in the portal:
+                requestTelemetry.Context.Properties["Overridden400s"] = "true";
+            }
+        }
+    }
+}
---
+++ AppInsights/Identifi.AppInsights/AIRequestTelemetryInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Claims;
+using Identifi.Essentials.Extensions;
+using Identifi.Infrastructure.Logging;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Identifi.AppInsights
+{
+    public class AIRequestTelemetryInitializer: ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            //Set sessionId/UserId and ServiceRequestId for each telemetry passed.
+            var sessionId  = ClaimsPrincipal.Current.GetSessionId();
+            var userId = ((ClaimsIdentity)ClaimsPrincipal.Current.Identity).GetStaffKey().ToString();
+            telemetry.Context.User.Id = userId;
+            telemetry.Context.Session.Id = !string.IsNullOrWhiteSpace(sessionId) ? sessionId : string.Empty;
+            telemetry.Context.Properties["SessionId"] =!string.IsNullOrWhiteSpace(sessionId) ? sessionId : string.Empty;
+            telemetry.Context.Properties["ServiceRequestId"] = IdentifiLogContext.CurrentServiceRequestId;
+        }
+    }
+}
